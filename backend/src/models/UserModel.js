0c2927965b22809e07e5530d12247727a2a6a8a6
Swayam Purwar <167647881+SwayamPurwar@users.noreply.github.com>
2026-02-14 06:16:58 +0530

    fix: improved db connection and user model middleware

--- backend/src/models/UserModel.js
+++ backend/src/models/UserModel.js
@@ -30,9 +30,12 @@ UserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     return next();
   }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next(); // Move next() inside the try block
+  } catch (error) {
+    next(error); // Pass error to the next middleware
+  }
 });
-
 module.exports = mongoose.model("User", UserSchema);
